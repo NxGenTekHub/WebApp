e2038b313d7774e318211d855056db8cab422cf6
NxGenTekHub <NxGenTekHub@users.noreply.github.com>
2023-02-21 00:20:35 +0000

    deploy: f9460230d32fa30b86bf16838ed197bf352a6604

---
+++ components/DownloadSection/DownloadSection.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useRef } from "react"
+import './DownloadSection.scss'
+
+const DownloadSection = () => {
+  const downloadImageRef = useRef()
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting)
+          entry.target.classList.add("fadeIn")
+      })
+    }, {
+      threshold: 0.5
+    })
+    observer.observe(downloadImageRef.current)
+  }, [])
+  
+  return (
+    <section className="downloadSection">
+      <div className="imageSection">
+        <img 
+          //src='../src/images/appsImage.png' 
+          className='downloadImage'
+          ref={downloadImageRef} 
+          width={0} height={0}/>
+      </div>
+      {/* <div className="downloadText">
+        <div className="downloadTitle">
+          Our Services Are Available Anytime, Anywhere.
+        </div>
+        <div className="downloadSubtitle">
+          Download the app today!
+        </div>
+        <div className="downloadButtons">
+        </div> 
+      </div>*/}
+    </section>
+  )
+}
+
+export default DownloadSection
---
+++ components/Footer/Footer.jsx
@@ -0,0 +1,83 @@
+import './Footer.scss'
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footerContainer">
+        <div className="footerColumn">
+          <div className="columnTitle">
+            Company
+            <hr className='columnTitleHR'/>
+          </div>
+          <div className="columnText">
+            <div className="columnTextItem">
+              <a href="#">About</a>
+            </div>
+            <div className="columnTextItem">
+              <a href="#">Services</a>
+            </div>
+            <div className="columnTextItem">
+              <a href="#">Community</a>
+            </div>
+            <div className="columnTextItem">
+              <a href="#">Blog</a>
+            </div>
+          </div>
+        </div>
+        <div className="footerColumn">
+          <div className="columnTitle">
+            Resources
+            <hr className='columnTitleHR'/>
+          </div>
+          <div className="columnText">
+            <div className="columnTextItem">
+              <a href="#">Help</a>
+            </div>
+            <div className="columnTextItem">
+              <a href="#">Terms</a>
+            </div>
+            <div className="columnTextItem">
+              <a href="#">Privacy</a>
+            </div>
+          </div>
+        </div>
+        <div className="footerColumn">
+          <div className="columnTitle">
+            Support
+            <hr className='columnTitleHR'/>
+          </div>
+          <div className="columnText">
+            <div className="columnTextItem">
+              <a href="#">Quick Search</a>
+            </div>
+            <div className="columnTextItem">
+              <a href="#">Popular Help Topics</a>
+            </div>
+            <div className="columnTextItem">
+              <a href="#">Product FAQs</a>
+            </div>
+          </div>
+        </div>
+        <div className="footerColumn">
+          <div className="columnTitle">
+            Contact
+            <hr className='columnTitleHR'/>
+          </div>
+          <div className="columnText">
+            <div className="columnTextItem">
+              Phone: +1 (443) 254-7691
+            </div>
+            <div className="columnTextItem">
+              Email: <u><a href='#'>hr@nxgentekhub.com</a></u>
+            </div>
+            <div className="columnTextItem">
+              Address:  5023 Crape Myrtle ct, <br/>Ellicott City, MD 21042
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
